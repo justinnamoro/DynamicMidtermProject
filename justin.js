74fed3b30ad169ac6ae7593f7968155d37461b9f
Justin Namoro <justinnamoro@JustinsMac.local>
2020-03-20 20:51:56 -0700

    story and functions completed

--- justin.js
+++ justin.js
@@ -11,6 +11,16 @@ function ballClick(){
 
 }
 
+function starClick(){
+    if (page_state === 5){
+        document.querySelector('#picture').style.backgroundImage = 'url(images/scene6.jpg)';
+        document.querySelector('#instructions').style.display = 'none';
+        document.querySelector('#next').style.display = 'inline';
+        document.querySelector('#item2').style.display = 'none';
+        document.getElementById('desc').innerHTML = 'Molly hissed at the dogs, "You have four legs too! You <br> should use them."';
+    }
+}
+
 
 
 // Change 
@@ -34,8 +44,18 @@ function nextPage(){
     else if (page_state === 4){
         document.querySelector('#picture').style.backgroundImage = 'url(images/scene5.jpg)';
         document.getElementById('desc').innerHTML = 'Jolly and his friends laugh at Molly. <br> "You are just a cat with four legs. <br> You cannot have fun with four legs!"';
+        document.querySelector('h2').style.display = 'inline';
+        document.querySelector('#item2').style.opacity = '100%';
+        document.querySelector('#next').style.display = 'none';
+        document.getElementById('instructions').innerHTML = 'Read the bottom sentences, then click on the blue power star of logic!';
         page_state = 5;
     }
 
+    else if (page_state === 5){
+        document.querySelector('#picture').style.backgroundImage = 'url(images/scene7.jpg)';
+        document.querySelector('#item3').style.opacity = '100%';
+        document.getElementById('desc').innerHTML = "The dogs realized Molly's logic, as they slowly place <br> their front legs on the ground. Molly then steals her ball back! <br> THE END!";
+        page_state = 6;
+    }
     
 }
